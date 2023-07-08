9b6c438c4508d52e94a42b166222bbc1c4a76fe0
ElectronAmpere <82173777+ElectronAmpere@users.noreply.github.com>
2023-07-08 16:22:14 +0530

    Updating for exercises in c

---
+++ source/examples/chapter_1/ex_1_1_hello_world.c
@@ -0,0 +1,29 @@
+/**
+ * @file ex_1_1_hello_world.c
+ * @author ElectronAmpere (learningthiruvalluvar@gmail.com)
+ * @brief Exercise 1.1 :
+ *              Run the "hello, world" program on your system. Experiment with leaving
+ *              out parts of the program, to see what error messages you get.
+ * @version 0.1
+ * @date 2023-06-26
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+#include <stdio.h>
+
+/**
+ * @brief main function program
+ * 
+ * @return int 
+ */
+int main()
+{
+    // The text given within the "<text>" is called a string literal
+    printf("Hello, ");
+    printf("World");
+    printf("\n"); // The "\n" is an escape sequence for newline insertion
+    
+    /**/
+    return (0);
+}
