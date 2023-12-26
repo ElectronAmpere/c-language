f9a6e380dda20c95e765461cc654ce5e9a60d061
ElectronAmpere <learningthiruvalluvar@gmail.com>
2023-12-26 16:10:45 +0530

    added reference

--- source/exercises/chapter_1/ex_1_20_detab.c
+++ source/exercises/chapter_1/ex_1_20_detab.c
@@ -4,6 +4,7 @@
  * @brief Exercise 1-20. Write a program detab that replaces tabs in the input with the proper number 
  * of blanks to space to the next tab stop. Assume a fixed set of tab stops, say every n columns. 
  * Should n be a variable or a symbolic parameter?
+ * @ref https://stackoverflow.com/questions/7178201/kr-exercise-1-20-need-some-clarification#7178297
  * @version 0.1
  * @date 2023-12-26
  * 
