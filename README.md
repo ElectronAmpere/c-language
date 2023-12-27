67308dee24bac09843ff5cd0bd2327855185802f
ElectronAmpere <learningthiruvalluvar@gmail.com>
2023-12-27 17:45:43 +0530

    Updated readme added ex.1-21 doesn't work need to modify

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # C Programming Language
+
+## Reference to solutions
+
+[C programming using K&R book](https://www.learntosolveit.com/)
+
+## Programming Exercises
+
+[Exercism](https://exercism.org/tracks/c)
+
+## Note
+
+Optimisation to all programs is not done, need to test and optimise them.
--- source/exercises/chapter_1/ex_1_21_entab.c
+++ source/exercises/chapter_1/ex_1_21_entab.c
@@ -11,3 +11,63 @@
  * @copyright Copyright (c) 2023
  * 
  */
+#include <stdio.h>
+
+#define TAB_STOPS       (4) /** Spaces to be replaced for Tabs */
+#define SPACES_TO_TAB   (3) /** Minimum number of spaces to be replaced as a tab */
+#define MAXLINE         (1000) /** Maximum line length */
+#define MAXLINE_ENDINGS (2) /** Appended line endings length */
+#define MAXLINE_TOTAL   (MAXLINE + MAXLINE_ENDINGS) /** Actual maximum line length */
+
+int exp_getline(char line[], int maxline);
+
+int main()
+{
+    char line[MAXLINE_TOTAL]; /** current input line */
+    int length; /** length of the line */
+
+    while((length = exp_getline(line, MAXLINE)) > 0)
+        printf("%s", line);
+
+    return 0;
+}
+
+int exp_getline(char line[], int maxline)
+{
+    int c, length, tabstops, spaces;
+
+    tabstops = TAB_STOPS;
+    spaces = 0;
+
+    for (length = 0; length < maxline && (c = getchar()) != EOF && c != '\n'; ++length)
+    {
+        --tabstops;
+        
+        if (c == ' ')
+        {
+            ++spaces;
+        }
+        else
+            line[length] = c,
+            spaces = 0;
+
+        if (spaces >= TAB_STOPS && tabstops >= SPACES_TO_TAB)
+            line[length] = '\t',
+            spaces = spaces - TAB_STOPS,
+            tabstops = 0;
+        else
+            line[length] = c;
+        
+        if (tabstops < 1)
+            tabstops = TAB_STOPS;
+    }
+
+    if (c == '\n')
+        line[length] = c,
+        ++length;
+
+    line[length] = '\0';
+    ++length;
+
+    return (length - MAXLINE_ENDINGS);
+}
